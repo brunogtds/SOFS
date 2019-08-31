bf6d42afc666b0f49234de230052a7f894b6b21f
Bruno Tavares <bruno.gtavares2011@gmail.com>
2019-08-31 17:28:11 -0300

    Logs gerados

--- component/scrape.js
+++ component/scrape.js
@@ -20,23 +20,42 @@ http://pesquisa.bvs.br/aps/?output=site&lang=pt&from=101&sort=&format=summary&co
 
 
 const rp= require('request-promise');
-const url= 'http://pesquisa.bvs.br/aps/?output=site&lang=pt&from=1&sort=&format=summary&count=100&fb=&page=1&filter%5Bdb%5D%5B%5D=SOF&q=diabetes&index=tw';
+//Diabetes1
+//const url= 'http://pesquisa.bvs.br/aps/?output=site&lang=pt&from=1&sort=&format=summary&count=100&fb=&page=1&filter%5Bdb%5D%5B%5D=SOF&q=diabetes&index=tw';
+//Diabetes2
+//const url= 'http://pesquisa.bvs.br/aps/?output=site&lang=pt&from=101&sort=&format=summary&count=100&fb=&page=2&filter%5Bdb%5D%5B%5D=SOF&q=diabetes&index=tw';
+//Hipertensão1
+const url= 'http://pesquisa.bvs.br/aps/?output=site&lang=pt&from=101&sort=&format=summary&count=100&fb=&page=2&filter%5Bdb%5D%5B%5D=SOF&q=hipertens%C3%A3o&index=tw&search_form_submit=Pesquisar';
+
 const $= require('cheerio');
 
 const sofsUrl=[];
 
+const fs = require('fs');
+const util = require('util');
+const log_file = fs.createWriteStream(__dirname + '/Hipertensao.log', {flags : 'w'}); //Alterar nome do arquivo log
+const log_stdout = process.stdout;
+
+//Override da função console.log para gerar arquivos log
+
+console.log = function(d) { //
+    log_file.write(util.format(d) + '\n');
+    log_stdout.write(util.format(d) + '\n');
+};
+
+
 rp(url).then(function (html) {
 
 
-    for (let i=0; i < 300; i++) {
+
+    for (let i=0; i < 332; i++) {
 
         sofsUrl.push({"Links das sofs": $('.record', html).find('a')[i].attribs.href.trim().split(" ")});
     }
 
 
-
-    console.log(sofsUrl);
-
+    
+  console.log(util.inspect (sofsUrl, {maxArrayLength: null}));
 
 
 
